a56b6e0871986e0df42990849289da8631c1aece
calosEmanuelSS <emanuelsan026@gmail.com>
2025-05-18 13:49:28 -0300

    Create attArray.js

---
+++ RevisãoG1/attArray.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')({})
+/* 1) Objetivo: Crie um array chamado frutas contendo pelo menos 5 frutas. Em seguida:
+    Adicione uma fruta ao final do array.
+    Remova a primeira fruta do array.
+    Exiba todas as frutas no console usando um loop for.
+
+var frutas = ['banana', 'abacate', 'manga', 'melancia', 'maça']
+
+console.log(`As frutas são: ${frutas.join(', ')}.`)
+
+var add = prompt('Digite uma fruta para adcionar a lista: ')
+frutas.push(add)
+frutas.shift()
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+} */
+
+/* 2) Crie um array com números de 1 a 20. Em seguida, utilize o método filter para criar um novo array contendo apenas os números pares. 
+Exiba o novo array no console.*/
+
+
+const num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
